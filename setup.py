7c7de84c54e450d9debda446cfcd8f39bf94741d
Brad Morgan <brad-morgan@comcast.net>
2020-06-25 15:25:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_displayz"
 plugin_name = "OctoPrint-DisplayZ"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.3.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
